6b761864212f8f372dfac3f24206b382c58eee1e
laneri <aneriluis@gmail.com>
2026-02-07 11:45:56 +0100

    Initial commit

---
+++ src/main/java/com/raccon/GymRank/GymRankApplication.java
@@ -0,0 +1,13 @@
+package com.raccon.GymRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymRankApplication.class, args);
+	}
+
+}
