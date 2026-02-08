b1a35dc39961ad27ff6be6ad8fb05d1dc2053809
Zormorych <zormorych@gmail.com>
2026-02-08 17:16:24 +0100

    pull request try 2

--- src/main/java/com/raccon/GymRank/infrastructure/web/HealthController.java
+++ src/main/java/com/raccon/GymRank/infrastructure/web/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//intento de pull request
+//intento de pull request en pull request branch
 @RestController
 @RequestMapping("/health")
 public class HealthController {
